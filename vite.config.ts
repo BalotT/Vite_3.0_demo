6cdc99567461faf87e7736e8b5fabb23481f0692
leijiaxin <jiaxin.lei@tophant@.com>
2022-07-29 09:58:24 +0800

    create project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueSetupExtend from 'vite-plugin-vue-setup-extend-plus';
+// 如果使用vite-plugin-vue-setup-extend插件 编译不会报错
+import ViteRestart from 'vite-plugin-restart';
 
+// vite-plugin-vue-setup-extend-plus和vite-plugin-restart插件在vite 版本2.9.0就可以使用
+// 除了vue(),其他大部分插件使用 ***() 都会报错 is not function
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+    plugins: [
+        vue(),
+        // vueSetupExtend(),  // vueSetupExtend.default() 就可以通过编译
+        // ViteRestart({
+        //     restart: [
+        //         'vite.config.ts',
+        //     ]
+        // })
+    ]
 })
